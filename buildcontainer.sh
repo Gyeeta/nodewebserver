1878ed378ebbc593a3ec99021c04b20aa7b3e746
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-11-02 13:10:02 +0530

    Force no cache
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- buildcontainer.sh
+++ buildcontainer.sh
@@ -2,5 +2,5 @@
 
 NODEWEBSERVER_VERSION="`./runwebserver.sh --version | awk '{print $NF}'`"
 
-DOCKER_BUILDKIT=1 docker build -t ghcr.io/gyeeta/nodewebserver:latest -t ghcr.io/gyeeta/nodewebserver:"$NODEWEBSERVER_VERSION" -f ./Dockerfile --build-arg NODEWEBSERVER_VERSION=v"${NODEWEBSERVER_VERSION}" .
+DOCKER_BUILDKIT=1 docker build -t ghcr.io/gyeeta/nodewebserver:latest -t ghcr.io/gyeeta/nodewebserver:"$NODEWEBSERVER_VERSION" -f ./Dockerfile --no-cache --build-arg NODEWEBSERVER_VERSION=v"${NODEWEBSERVER_VERSION}" .
 
