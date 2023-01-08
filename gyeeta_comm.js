035afc7072c9e3356135802b1856b2cbbd8bf94f
Krishna <35102612+kvdevel@users.noreply.github.com>
2023-01-08 17:17:37 +0530

    Version change to v0.2.0
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- gyeeta_comm.js
+++ gyeeta_comm.js
@@ -35,10 +35,10 @@ const JsonMsgTypes = {
 const		RESP_WEB_JSON		= 1000;
 const 		RESP_JSON_WITH_HEADER	= 1;
 
-const		NODE_VERSION		= 0x000100;
-const		NODE_VERSION_STR	= '0.1.0';
-const		MIN_SHYAMA_VERSION	= 0x000100;
-const		MIN_MADHAVA_VERSION	= 0x000100;
+const		NODE_VERSION		= 0x000200;
+const		NODE_VERSION_STR	= '0.2.0';
+const		MIN_SHYAMA_VERSION	= 0x000200;
+const		MIN_MADHAVA_VERSION	= 0x000200;
 const		COMM_VERSION		= 1;
 const		CLI_TYPE_REQ_RESP	= 0;
 
@@ -1568,7 +1568,7 @@ class ShyamaHandler
 
 						const			currmsec = Date.now();
 
-						if (currmsec - this.compquerymsec_ > 10 * 60 * 1000) {
+						if (currmsec - this.compquerymsec_ > 5 * 60 * 1000) {
 							this.lastchgmsec_ 	= 0;
 							this.compquerymsec_ 	= currmsec;
 						}
